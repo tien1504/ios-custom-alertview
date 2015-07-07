97ad9211b1199ce0967df1d3e54dbd9bfcfceb78
MacBook <macbook@Tiens-MacBook-Pro.local>
2015-07-07 13:55:47 +0700

    Add share title/description custom view

--- CustomIOSAlertView/CustomIOSAlertView/ViewController.m
+++ CustomIOSAlertView/CustomIOSAlertView/ViewController.m
@@ -42,10 +42,10 @@
     CustomIOSAlertView *alertView = [[CustomIOSAlertView alloc] init];
 
     // Add some custom content to the alert view
-    [alertView setContainerView:[self createDemoView]];
+    [alertView setContainerView:[self createShareDialogWithTitle: @"Share to Facebook"]];
 
     // Modify the parameters
-    [alertView setButtonTitles:[NSMutableArray arrayWithObjects:@"Close1", @"Close2", @"Close3", nil]];
+    [alertView setButtonTitles:[NSMutableArray arrayWithObjects:@"Cancel", @"Share", nil]];
     [alertView setDelegate:self];
     
     // You may use a Block, rather than a delegate.
@@ -66,13 +66,63 @@
     [alertView close];
 }
 
-- (UIView *)createDemoView
+- (UIView *)createShareDialogWithTitle: (NSString*) shareTitle
 {
-    UIView *demoView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 290, 200)];
-
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 270, 180)];
-    [imageView setImage:[UIImage imageNamed:@"demo"]];
-    [demoView addSubview:imageView];
+    UIView *demoView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 290, 180)];
+    
+    UIFont* smallFont = [UIFont fontWithDescriptor:[UIFontDescriptor preferredFontDescriptorWithTextStyle:UIFontTextStyleFootnote] size:11];
+    UIFont* normalFont = [UIFont fontWithDescriptor:[UIFontDescriptor preferredFontDescriptorWithTextStyle:UIFontTextStyleBody] size:13];
+    
+    //title
+    UILabel* dialogTitle = [UILabel new];
+    [dialogTitle setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [demoView addSubview:dialogTitle];
+    [dialogTitle setText:shareTitle];
+    [dialogTitle setTextAlignment:NSTextAlignmentCenter];
+    dialogTitle.font = [UIFont fontWithDescriptor:[UIFontDescriptor preferredFontDescriptorWithTextStyle:UIFontTextStyleHeadline] size:14];
+    
+    
+    //video title
+    UILabel* uploadTitle = [UILabel new];
+    [uploadTitle setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [demoView addSubview:uploadTitle];
+    [uploadTitle setText:@"Title"];
+    [uploadTitle setFont:smallFont];
+    
+    UITextField* titleField = [UITextField new];
+    [titleField setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [demoView addSubview:titleField];
+    [titleField setBorderStyle:UITextBorderStyleRoundedRect];
+    [titleField setFont:normalFont];
+    
+    
+    //description
+    UILabel* uploadDescription = [UILabel new];
+    [uploadDescription setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [uploadDescription setText:@"Description"];
+    [demoView addSubview:uploadDescription];
+    [uploadDescription setFont:smallFont];
+    
+    
+    UITextView* textView = [UITextView new];
+    [textView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [demoView addSubview:textView];
+    [textView.layer setCornerRadius:8.0];
+    [textView.layer setBorderColor:[UIColor colorWithRed:200.0/255.0 green:200.0/255.0 blue:200.0/255.0 alpha:1.0].CGColor];
+    [textView.layer setBorderWidth:0.5];
+    
+    NSDictionary* dict = NSDictionaryOfVariableBindings(dialogTitle, uploadTitle, titleField, uploadDescription ,textView);
+    [demoView addConstraints: [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-10-[dialogTitle]" options:0 metrics:nil views:dict]];
+    [demoView addConstraint:[NSLayoutConstraint constraintWithItem:dialogTitle attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:demoView attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0.0]];
+    
+    //constraint for titleField
+    [demoView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[dialogTitle]-20-[titleField]-10-[textView]-10-|" options:0 metrics:nil views:dict]];
+    [demoView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[uploadTitle(60)]-[titleField]-|" options:0 metrics:nil views:dict]];
+    [demoView addConstraint:[NSLayoutConstraint constraintWithItem:uploadTitle attribute:NSLayoutAttributeBaseline relatedBy:NSLayoutRelationEqual toItem:titleField attribute:NSLayoutAttributeBaseline multiplier:1.0 constant:0.0]];
+    
+    //constraint for textView
+    [demoView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[uploadDescription(60)]-[textView]-|" options:0 metrics:nil views:dict]];
+    [demoView addConstraint:[NSLayoutConstraint constraintWithItem:uploadDescription attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:textView attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0]];
 
     return demoView;
 }
